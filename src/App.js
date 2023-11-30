66de1b0c0574352cbaa9dcd6350bc88adc76a802
KOO612 <ksm99007@gmail.com>
2023-11-30 17:43:21 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동', '파이썬']);
+  let [date, setDate] = useState('2월 17일 발행');
+  let [num, setNum] = useState(0);
+
+  const onClick = () => {
+    setNum(num + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]} <span onClick={onClick}>👍</span> {num}
+        </h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>{date}</p>
+      </div>
     </div>
   );
 }
